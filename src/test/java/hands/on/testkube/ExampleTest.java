3f9d489831b92af2eab5b91b6e64384e342ab294
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-03-17 09:00:02 +0100

    Added example unit test.

---
+++ src/test/java/hands/on/testkube/ExampleTest.java
@@ -0,0 +1,11 @@
+package hands.on.testkube;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleTest {
+    @Test 
+    void aTestMethod() {
+        assertTrue(true, "Test should be true");
+    }
+}
